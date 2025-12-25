da90266631729fc256e3e5905b4185747d233f1d
mohammad <mgharebaghi92@gmail.com>
2025-12-25 23:27:58 +0330

    fix origin problem for nginx proxy

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["mygreenhouses.ir", "www.mygreenhouses.ir", "localhost:3000"],
+    },
+  },
 };
 
 export default nextConfig;
