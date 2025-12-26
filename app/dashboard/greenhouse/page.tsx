182f335f89b6bee4d2762ab4c8a2663f32da02d4
mohammad <mgharebaghi92@gmail.com>
2025-12-26 13:17:13 +0330

    pages loading added

--- app/dashboard/greenhouse/page.tsx
+++ app/dashboard/greenhouse/page.tsx
@@ -4,7 +4,5 @@ import { Greenhouses as GreenHouse } from "@/app/generated/prisma/client";
 
 export default async function GreenHousesPage() {
   const initialData: GreenHouse[] = await allGreenHouses();
-  // Artificial delay to test loading UI
-  await new Promise((resolve) => setTimeout(resolve, 3000));
   return <GreenHouses initialData={initialData} />;
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 import faIR from "antd/locale/fa_IR";
 
+import { Suspense } from "react";
 import { ConfigProvider } from "antd";
 import NavigationProgressBar from "./components/NavigationProgressBar";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ConfigProvider locale={faIR} direction="rtl" theme={{ token: { fontFamily: "IransansR" } }}>
-      <NavigationProgressBar />
+      <Suspense fallback={null}>
+        <NavigationProgressBar />
+      </Suspense>
       {/* <GlobalFaDigits /> */}
       {children}
     </ConfigProvider>
