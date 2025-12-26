520080f4bfa7684839cae7cae80a10b2b4492a1a
mohammad <mgharebaghi92@gmail.com>
2025-12-26 11:29:25 +0330

    pages loading added

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa">
+    <html lang="fa" suppressHydrationWarning>
       <body className={`antialiased`}>
         <Providers>{children}</Providers>
       </body>
