0b19e6ba8ff9e47530d2678dc1fa6c61e6a86c3b
mohammad <mgharebaghi92@gmail.com>
2025-12-27 12:55:41 +0330

    update next & react for security reason

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   experimental: {
     serverActions: {
       allowedOrigins: ["mygreenhouses.ir", "www.mygreenhouses.ir", "localhost:3000"],
